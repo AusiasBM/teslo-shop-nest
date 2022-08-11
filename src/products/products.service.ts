30d5e379c4bd0b7b1f5667f6f16eb25f1c99ef5d
AusiasBM <ausiasbm@gmail.com>
2022-08-11 12:31:10 +0200

    post creado con revision de slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  private readonly logger = new Logger('ProductService'); // Con esto conseguimos que los errores sean más legibles en el log
+  
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+  ){}
+  
+  
+  async create(createProductDto: CreateProductDto) {
+    
+    try {
+
+      
+
+      const product = this.productRepository.create(createProductDto); // Esto solo crea la instancia del producto ( es síncrono )
+      await this.productRepository.save( product );
+
+      return product;
+
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+    
   }
 
   findAll() {
@@ -23,4 +50,15 @@ export class ProductsService {
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
+  // Con este método controlamos todos los errores centralizados
+  private handleDBExceptions( error: any ){
+
+    if( error.code === '23505' )
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error)
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+
 }
