3f2139b68e7f5881c19ef3e032401f2fdd5e1bde
AusiasBM <ausiasbm@gmail.com>
2022-08-11 11:13:55 +0200

    BBDD, env - Configurados

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true, // Carga automáticamente las entidades que vamos definiendo
+      synchronize: true, // En producción se suele poner en false
+    })
+  ],
+})
+export class AppModule {}
