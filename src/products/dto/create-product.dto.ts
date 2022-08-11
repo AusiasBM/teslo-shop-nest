33bdadc8a5029f454a28cd33257d59d47fdfbbe9
AusiasBM <ausiasbm@gmail.com>
2022-08-11 12:02:56 +0200

    dto create inicial creado

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,34 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    descripcion?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true }) // Cada elemento del array tiene que cumplir la condición
+    @IsArray()
+    sizes: string[];
+
+    @IsIn([ 'men', 'women', 'kid', 'unisex']) // Me tiene que enviar algún valor que esté aquí
+    gender: string;
+
+}
